cc19d79a52f70c20d9f016ef149205a8a218b5a6
Jehan-Guillaume (ioguix) de Rorthais <jgdr@dalibo.com>
2011-02-25 21:46:08 +0100

    Add support for either listening directly from network or opening a pcap file
    Plus:
      * suppress some ending spaces all over the place...
    	* add option --port, --read & --interface
    	* rename option --plugin as --output

--- pgshark.pl
+++ pgshark.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 
 use strict;
 use warnings;
@@ -14,6 +14,9 @@ use Getopt::Long;
 use Pod::Usage;
 use pgShark::Utils;
 
+## TODO
+# * Does not support authentication dissection
+
 =head1 pgshark.pl
 
 pgshark.pl - Mess with PostgreSQL client's traffic
@@ -22,37 +25,53 @@ pgshark.pl - Mess with PostgreSQL client's traffic
 
 =over 2
 
-=item * pgshark.pl --help
+=item pgshark.pl --help
 
-=item * pgshark.pl [--debug] {--plugin plugin_name}
+=item pgshark.pl [--debug] [--read file] {--output plugin_name}
 
 Where B<plugin_name> could be I<sql> or I<normalize>.
 
 =back
-  
+
 =head1 DESCRIPTION
 
 This program study PostgreSQL traffic captured in tcpdump format and is able to make various things with extracted client's
 activities.
 
-B<pgshark> comes with various plugins to do various things with client's traffic.
+B<pgshark> comes with various output plugins to do various things with PostgreSQL client's traffic.
 
 Presently, B<pgshark> is only able to read tcpdump files from its standart input.
 
 =over 2
 
-=item * B<-h>, B<--help>
-
-Show this help message and exit.
-
-=item * B<-d>, B<--debug> 
+=item B<-d>, B<--debug>
 
 Print some debug messages to the standart error. The more you repeat this option, the more B<pgshark> will be verbose.
 (well, presently, only one level of debug from core only...)
 
-=item * B<-p>, B<--plugin>
+=item B<--help>
 
-Select the traffic processing plugin. See section L</PLUGINS>.
+Show this help message and exit.
+
+=item B<--i>, B<--interface> <interface name>
+
+Capture PostgreSQL traffic directly from the given network interface. Conflict with B<--read>.
+By default, B<pgshark> will read from stdin if neither B<--read> or B<--interface> are given.
+
+=item B<-o>, B<--output> <plugin name>
+
+Select the traffic processing output plugin. This parameter value is case-insensitive
+(eg. SQL, Sql and sql wil all select the SQL plugin output).
+See section L</PLUGINS>.
+
+=item B<-p>, B<--port> <port>
+
+Give the port the PostgreSQL backend is listening on.
+
+=item B<-r>, B<--read> <path to file>
+
+Read PostgreSQL traffic from given pcap file. Conflict with B<--interface>.
+By default, B<pgshark> will read from stdin if neither B<--read> or B<--interface> are given.
 
 =back
 
@@ -60,14 +79,14 @@ Select the traffic processing plugin. See section L</PLUGINS>.
 
 =over 2
 
-=item * B<sql>
+=item B<sql>
 
-The B<sql> plugin write captured queries on stdout. Because of limitation of SQL language it doesn't support unnamed 
+The B<sql> plugin write captured queries on stdout. Because of limitation of SQL language it doesn't support unnamed
 prepared statement, so it actually name them.
 
 Presently, this plugin doesn't support cursors.
 
-=item * B<normalize>
+=item B<normalize>
 
 The B<normalize> will try to normalize queries and prepared queries and output them to stdoud. It aims to give you a list
 of unique queries, however the number of time they has been send by clients and whatever their parameters were.
@@ -76,7 +95,21 @@ of unique queries, however the number of time they has been send by clients and 
 
 =head1 EXAMPLES
 
-cat some_capture.pcap | pgshark.pl --plugin SQL
+=over 2
+
+=item C<cat some_capture.pcap* | pgshark.pl --output SQL>
+
+Output all queries found in files C<some_capture.pcap*> in SQL to the standart output.
+
+=item C<pgshark.pl --output SQL -r some_capture.pcap001>
+
+Output all queries found in file C<some_capture.pcap001> in SQL to the standart output.
+
+=item C<pgshark.pl --output normalize -i eth0>
+
+Capture PostgreSQL traffic from interface eth0 and output normalized queries to the standart output.
+
+=back
 
 =head1 Author
 
@@ -107,24 +140,36 @@ sub longusage {
 my %args = (
 	debug => 0,
 	help => 0,
-	plugin => ''
+	interface => '',
+	output => '',
+	port => 5432,
+	read => '',
 );
 
+Getopt::Long::Configure('bundling');
 GetOptions(\%args, qw{
+	debug|d+
 	help
-	debug+
-	plugin=s
+	interface|i=s
+	output|o=s
+	port|p=s
+	read|r=s
 }) or usage();
 
-longusage() if ($args{help});
-usage("Argument --plugin is mandatory.\n") if ($args{plugin} eq '' );
 
-$args{plugin} = ucfirst lc $args{plugin};
+longusage() if ($args{help});
+
+usage("Argument --output is mandatory.\n") if $args{output} eq '';
+
+usage("Arguments --interface and --read are incompatible.\nEither listen from the networkor open a pcap file.\n")
+	if $args{interface} ne '' and $args{read} ne '';
+
+$args{output} = ucfirst lc $args{output};
 
 # check if given plugin name exist (avoid loading potential dangerous external unknown files)
-usage("This plugin does not exist.\n") if ( not (
-	($args{plugin} eq 'Sql')
-	or ($args{plugin} eq 'Normalize')
+usage("This output plugin does not exist.\n") if ( not (
+	($args{output} eq 'Sql')
+	or ($args{output} eq 'Normalize')
 ));
 
 # set debug level given in options
@@ -133,25 +178,57 @@ set_debug($args{debug});
 debug (1, "Options:\n%s\n", Dumper(\%args));
 
 # load the plugin
-require "./pgShark/$args{plugin}.pm";
+require "./pgShark/$args{output}.pm";
 
-my $processor = $args{plugin}->new(\%args);
+my $processor = $args{output}->new(\%args);
 
-# opening the pcap file 
-# TODO support input file ?
-# TODO support direct sniffing ?
-$pcap = pcap_open_offline('-', \$err);
+## opening the pcap handler
+# open a live capture on given interface
+if ($args{interface} ne '') {
+
+	unless ($pcap = pcap_open_live($args{interface}, 65535, 0, 0, \$err) ) {
+		debug (0, "Can not open interface '%s':\n", $args{interface});
+		die ($err);
+	}
+	my $filter = undef;
+
+	# the following filter reject TCP-only stuff and capture only frontend messages
+	pcap_compile($pcap, \$filter,
+		"tcp and dst port $args{port} and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)", 0, 0
+	);
+	pcap_setfilter($pcap, $filter);
+
+	debug(1, "Listening from network on interace '%s'.\n", $args{interface});
+}
+# we have no interface to listen on,
+# either open given pcap file to read from or start reading from stdin
+else {
+	# read from stdin if no pcap file given
+	$args{read} = '-' if $args{read} eq '';
+
+	unless ($pcap = pcap_open_offline($args{read}, \$err)) {
+		debug (0, "Can not read from file '%s':\n", $args{read});
+		die ($err);
+	}
+
+	if ($args{read} eq '-') {
+		debug(1, "Reading Pcap data from stdin.\n");
+	}
+	else {
+		debug(1, "Reading from Pcap file '%s'.\n", $args{read});
+	}
+}
 
 while (defined($pckt = pcap_next($pcap, \%pckt_hdr))) {
-	
+
 	$pckt_num++;
 	my ($eth, $ip, $tcp);
 	my ($sess_hash);
-	
+
 	$eth = NetPacket::Ethernet->decode($pckt);
-	
-	if (defined($eth->{data}) 
-	and defined($eth->{type}) 
+
+	if (defined($eth->{data})
+	and defined($eth->{type})
 	and ($eth->{type} == ETH_TYPE_IP)) {
 		# decode the IP payload
 		$ip = NetPacket::IP->decode($eth->{data});
@@ -159,7 +236,7 @@ while (defined($pckt = pcap_next($pcap, \%pckt_hdr))) {
 		if ($ip->{proto} == IP_PROTO_TCP) {
 			# decode the TCP payload
 			$tcp = NetPacket::TCP->decode($ip->{data});
-			# we could add "$ip->{dest_ip}$tcp->{dest_port}" to this hash, 
+			# we could add "$ip->{dest_ip}$tcp->{dest_port}" to this hash,
 			# but we are suppose to work with only one server
 			$sess_hash = $ip->{src_ip} . $tcp->{src_port};
 			$sess_hash =~ s/\.//g; # useless but for better debug messages
@@ -167,10 +244,10 @@ while (defined($pckt = pcap_next($pcap, \%pckt_hdr))) {
 
 		# check if we have data
 		if (length $tcp->{data}) {
-			
+
 			debug(2, "packet: #=%d len=%s, caplen=%s\n", $pckt_num, map { $pckt_hdr{$_} } qw(len caplen));
 			debug(2, "IP:TCP %s:%d -> %s:%d\n", $ip->{src_ip}, $tcp->{src_port}, $ip->{dest_ip}, $tcp->{dest_port});
-			
+
 			if (! defined($sessions->{$sess_hash}) ) {
 				# we are opening a new pg session, wait for a valid message type from frontend
 				if (pack('A', $tcp->{data}) =~ /[BCdcfDEHFPpQSX]/) {
@@ -181,77 +258,77 @@ while (defined($pckt = pcap_next($pcap, \%pckt_hdr))) {
 					};
 				}
 			}
-			
+
 			# if we have a session with data
 			if (defined($sessions->{$sess_hash}) ) {
 				# the session is already authenticated we should get type'd messages
 				$sessions->{$sess_hash}->{data} .= $tcp->{data};
 				my $data_len = length $sessions->{$sess_hash}->{data};
-								
+
 				# if we have at least 5 byte, we can analyze the begin of message
 				while ($data_len >= 5) {
-					
+
 					# hash about message informations
 					my $pg_msg = {
 						sess_hash => $sess_hash
 					};
 					($pg_msg->{type}, $pg_msg->{len}) = unpack('AN', $sessions->{$sess_hash}->{data});
-					
+
 					# pg_len is the size of the message length field + data. it doesn't include the message type char
 					# so a full pgsql message is pg_len + 1
 					if ($data_len >= $pg_msg->{len} + 1) {
 						# we have enough data for a message
-						
+
 						debug(2, "    PGSQL: pckt=%d session=%s type=%s, len=%d, data_len=%d\n", $pckt_num, $sess_hash,
 							$pg_msg->{type}, $pg_msg->{len}, $data_len
 						);
 						$pg_msg->{data} = substr($sessions->{$sess_hash}->{data}, 5, $pg_msg->{len} - 4);
-						
+
 						SWITCH: {
 							# message: P
 							#   name=String
-							#   query=String 
+							#   query=String
 							#   nun_params=int16
-							#   params_types[]=int32[nb_formats] 
+							#   params_types[]=int32[nb_formats]
 							if ( $pg_msg->{type} eq 'P') {
 								my @params_types;
 								($pg_msg->{name}, $pg_msg->{query},
 									$pg_msg->{num_params}, @params_types
 								) = unpack('Z*Z*nN*', $pg_msg->{data});
 								$pg_msg->{params_types} = [@params_types];
-								
+
 								$processor->process_parse($pg_msg);
 								last SWITCH;
 							}
-							
+
 							# message: B
-							#   portal=String 
+							#   portal=String
 							#   name=String
-							#   nun_formats=int16 
-							#   formats[]=int16[nb_formats] 
-							#   nun_params=int16 
+							#   nun_formats=int16
+							#   formats[]=int16[nb_formats]
+							#   nun_params=int16
 							#   params[]=(len=int32,value=char[len])[nb_params]
 							if ( $pg_msg->{type} eq 'B') {
 								my @params_formats;
 								my @params;
 								my $msg = $pg_msg->{data};
-								
+
 								($pg_msg->{portal}, $pg_msg->{name}, $pg_msg->{num_formats}) = unpack('Z* Z* n', $msg);
 								# we add 1 bytes for both portal and name that are null-terminated
 								# + 2 bytes of int16 for $num_formats
 								$msg = substr($msg, length($pg_msg->{portal})+1 + length($pg_msg->{name})+1 +2);
-								
+
 								# catch formats and the $num_params as well
 								@params_formats = unpack("n$pg_msg->{num_formats} n", $msg);
 								$pg_msg->{num_params} = pop @params_formats;
 								$pg_msg->{params_types} = [@params_formats];
-								
+
 								$msg = substr($msg, ($pg_msg->{num_formats}+1) * 2);
-								
+
 								for (my $i=0; $i < $pg_msg->{num_params}; $i++) {
 									# unpack hasn't 32bit signed network template, so we use l>
 									my ($len) = unpack('l>', $msg);
-									
+
 									# if len < 0; the value is NULL
 									if ($len > 0) {
 										push @params, unpack("x4 a$len", $msg);
@@ -265,61 +342,61 @@ while (defined($pckt = pcap_next($pcap, \%pckt_hdr))) {
 										push @params, undef;
 										$msg = substr($msg, 4);
 									}
-									
+
 								}
-								
+
 								$pg_msg->{params} = [@params];
 
 								$processor->process_bind($pg_msg);
 								last SWITCH;
 							}
-							
+
 							# message: E
 							#   name=String
 							#   nb_rows=int32
 							if ( $pg_msg->{type} eq 'E') {
 								($pg_msg->{name}, $pg_msg->{nb_rows}) = unpack('Z*N', $pg_msg->{data});
-								
+
 								$processor->process_execute($pg_msg);
 								last SWITCH;
 							}
-							
+
 							# message: C
 							#   type=char
 							#   name=String
 							if ( $pg_msg->{type} eq 'C') {
-								
+
 								($pg_msg->{type}, $pg_msg->{name}) = unpack('AZ*', $pg_msg->{data});
-	
+
 								$processor->process_close($pg_msg);
 								last SWITCH;
 							}
-							
+
 							# message: Q
 							#    query=String
 							if ( $pg_msg->{type} eq 'Q') {
-								
+
 								# we remove the last char:
 								# query are null terminated in pgsql proto and pg_len includes it
 								$pg_msg->{query} = substr($pg_msg->{data}, 0, -1);
-								
+
 								$processor->process_query($pg_msg);
 								last SWITCH;
 							}
-							
+
 							# message: X
 							if ( $pg_msg->{type} eq 'X') {
 								$processor->process_disconnect($pg_msg);
 								last SWITCH;
 							}
-							
+
 							debug(2,"ignoring message type: %s\n", $pg_msg->{type});
 						}
 
 						### end of processing, remove processed data
 						$sessions->{$sess_hash}->{data} = substr($sessions->{$sess_hash}->{data}, 1 + $pg_msg->{len});
 						$data_len = length $sessions->{$sess_hash}->{data};
-						
+
 						$num_queries++;
 					}
 					else {
@@ -329,7 +406,7 @@ while (defined($pckt = pcap_next($pcap, \%pckt_hdr))) {
 					}
 				}
 
-				# remove the session if we have no trailing data. It helps keeping the code 
+				# remove the session if we have no trailing data. It helps keeping the code
 				# simple while tracking data splitted between many frame.
 				# we can do way much better by tracking the session disconnection.
 				if ($data_len == 0) {
